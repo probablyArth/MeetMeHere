ff950e9ab3961bd09be512fe60302b4a7085b72a
probablyarth <arththapa7@gmail.com>
2023-03-09 18:37:26 +0530

    Stupid lmao

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,15 +4,7 @@ import Header from "~/components/Header/index";
 import OverviewTab from "~/components/Tabs/Overview";
 import PreviousMeetingsTab from "~/components/Tabs/PreviousMeetings";
 
-const handleTabs = (currentTab: 0 | 1 | 2) => {
-  switch (currentTab) {
-    case 0:
-      return <OverviewTab />;
-    case 1:
-      return <PreviousMeetingsTab />;
-    case 2:
-  }
-};
+const tabs = [<OverviewTab />, <PreviousMeetingsTab />];
 
 const Dashboard: NextPage = () => {
   const [currentTab, setCurrentTab] = useState<0 | 1 | 2>(0);
@@ -20,7 +12,7 @@ const Dashboard: NextPage = () => {
   return (
     <main className="relative flex w-screen flex-col items-center justify-center">
       <Header />
-      {handleTabs(currentTab)}
+      {tabs[currentTab]}
     </main>
   );
 };
